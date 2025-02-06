7e16889f4dd3139659455dc93d76ff7f90581e1d
Ritzsizl <ritzraha4u@gmail.com>
2025-02-07 02:31:11 +0530

    login and logout functionality

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -2,7 +2,7 @@ import { generateToken } from "../../utils/generateToken.js";
 import User from "../models/User.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req,res) => {
+export const signup = async (req, res) => {
   const { email, password, username } = req.body;
   try {
     if (!password || !email || !username) {
@@ -43,4 +43,45 @@ export const signup = async (req,res) => {
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged In Successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login function");
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("token");
+  res
+    .status(200)
+    .json({ success: true, message: "User Logged Out Successfully" });
 };
